7e4219de662caab3fa002fb969040c8fe3955821
wuhuabin <13804087628@163.com>
2016-12-30 11:45:26 +0800

    finish main view render

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,19 +4,42 @@ require('styles/App.css');
 import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
+let images = [
+  require('../images/1cFEZZ8-one-piece-wallpaper-1920x1080.jpg'),
+  require('../images/606300.jpg'),
+  require('../images/650008.jpg'),
+  require('../images/cropped-1920-1080-616945.jpg'),
+  require('../images/funny-one-piece_011524997_277.jpg')
+]
+
+class ImgFigure extends React.Component{
+  render(){
+    return(
+      <figure>
+        <img src={this.props.src} style={{height:100}}/>
+        <figcaption>
+          <h2></h2>
+        </figcaption>
+      </figure>
+    )
+  }
+}
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className="index">
-        <img src={yeomanImage} alt="Yeoman Generator" />
-        <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <section className="stage">
+        <section className="img-sec">
+          {images.map((v,i)=><ImgFigure key={i} src={v} />)}
+        </section>
+        <nav className="controller-nav">
+
+        </nav>
+      </section>
     );
   }
 }
 
-AppComponent.defaultProps = {
-};
+AppComponent.defaultProps = {};
 
 export default AppComponent;
